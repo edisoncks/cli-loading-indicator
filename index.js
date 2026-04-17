de619f8bf0e8c0c27b33f391ca71acd11bb4daa1
Edison Chong <kurai.no.yozora@gmail.com>
2026-04-17 22:21:42 +0800

    fix: rewrite index.js with modern JavaScript syntax
    - Convert constructor function to ES2022 class
    - Use private class fields (#) for internal state
    - Fix static private method bug (was incorrectly static)
    
    Fixes receiver must be class LoadingIndicator error when calling
    #resetLineAndCursor as instance method.

--- index.js
+++ index.js
@@ -2,63 +2,71 @@
 
 import readline from "readline";
 
-// Braille patterns from:
-// http://symbolcodes.tlt.psu.edu/bylanguage/braillechart.html
-const PATTERNS = {
-  small: ["⠟", "⠯", "⠷", "⠾", "⠽", "⠻"],
-  large: ["⡿", "⣟", "⣯", "⣷", "⣾", "⣽", "⣻", "⢿"],
-};
+class LoadingIndicator {
+  // Braille patterns from:
+  // http://symbolcodes.tlt.psu.edu/bylanguage/braillechart.html
+  static PATTERNS = {
+    small: ["⠟", "⠯", "⠷", "⠾", "⠽", "⠻"],
+    large: ["⡿", "⣟", "⣯", "⣷", "⣾", "⣽", "⣻", "⢿"],
+  };
 
-function LoadingIndicator(attributes) {
-  attributes = attributes || {};
-  this.size = attributes.size || "large";
-  this.patterns = [...PATTERNS[this.size]];
-  this.interval = attributes.interval || 70;
-  this.format =
-    attributes.format ||
-    function (pattern) {
-      return pattern;
-    };
-  if (attributes.rotation === "cw") {
-    this.patterns.reverse();
+  // Public properties (required for backwards compatibility)
+  size;
+  patterns;
+  interval;
+  format;
+
+  // Private properties
+  #readlineInterface;
+  #animationInterval;
+  #patternIndex = 0;
+
+  constructor(attributes = {}) {
+    this.size = attributes.size ?? "large";
+    this.patterns = [...LoadingIndicator.PATTERNS[this.size]];
+    this.interval = attributes.interval ?? 70;
+    this.format = attributes.format ?? ((pattern) => pattern);
+
+    if (attributes.rotation === "cw") {
+      this.patterns.reverse();
+    }
   }
-}
 
-LoadingIndicator.prototype.start = function () {
-  this.readlineInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  process.stdout.write("\x1b[?25l");
-  this.patternIndex = 0;
-  this._animationInterval = setInterval(animate.bind(this), this.interval);
-};
-
-LoadingIndicator.prototype.stop = function () {
-  resetLineAndCursor();
-  process.stdout.write("\x1b[?25h");
-  this.readlineInterface.close();
-  if (this._animationInterval) {
-    clearInterval(this._animationInterval);
-    this._animationInterval = null;
+  start() {
+    this.#readlineInterface = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    process.stdout.write("\x1b[?25l");
+    this.#patternIndex = 0;
+    this.#animationInterval = setInterval(() => this.#animate(), this.interval);
   }
-};
 
-function animate() {
-  resetLineAndCursor();
-  this.readlineInterface.output.write(
-    this.format(this.patterns[this.patternIndex], this.patternIndex),
-  );
-  if (this.patternIndex < this.patterns.length - 1) {
-    this.patternIndex++;
-  } else {
-    this.patternIndex = 0;
+  stop() {
+    this.#resetLineAndCursor();
+    process.stdout.write("\x1b[?25h");
+    this.#readlineInterface.close();
+    if (this.#animationInterval) {
+      clearInterval(this.#animationInterval);
+      this.#animationInterval = null;
+    }
   }
-}
 
-function resetLineAndCursor() {
-  readline.clearLine(process.stdout, 0);
-  readline.cursorTo(process.stdout, 0);
+  #animate() {
+    this.#resetLineAndCursor();
+    this.#readlineInterface.output.write(
+      this.format(this.patterns[this.#patternIndex], this.#patternIndex),
+    );
+    this.#patternIndex =
+      this.#patternIndex < this.patterns.length - 1
+        ? this.#patternIndex + 1
+        : 0;
+  }
+
+  #resetLineAndCursor() {
+    readline.clearLine(process.stdout, 0);
+    readline.cursorTo(process.stdout, 0);
+  }
 }
 
 export default LoadingIndicator;
